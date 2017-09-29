4681b54658aa60a1dfb4182523b8d7096b631572
dd0s <pvvner@yandex.ru>
2017-09-29 19:26:49 +0300

    Work is going on. Gotta report a bug to original repository.

--- mac.c
+++ mac.c
@@ -1,70 +1,124 @@
+// BUG FOUND: IP is not ever explicitly initialized in the code
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-bool running = true;
+#define STACK_SIZE 256
+static int stack[STACK_SIZE];
 
-// Instruction pointer. The program itself is stored as an array of integers. 
-// The ip variable serves as an index in the array as to which instruction is currently being 
-// executed.
-int ip = 0;
-
-// stack pointer
-int sp = -1;
-
-int stack[256];
-
+// Instructions
 typedef enum {
+    HLT,
     PSH,
-    ADD,
     POP,
-    HLT
-} instructions;
-
-const int program[] = {
-    PSH, 5,
-    PSH, 6,
     ADD,
-    POP,
-    HLT
-};
+    MUL,
+    DIV,
+    SUB,
+    SLT,
+    MOV,
+    SET,
+    LOG,
+    IF,
+    IFN,
+    GLD,
+    GPT,
+    NOP
+} Instructions;
 
-int fetch() {
-    return program[ip];
-}
+// Registers
+typedef enum {
+    A, B, C, D, E, F, I, J, // general purpose
+    EX,                     // excess
+    EXA,                    // more excess
+    IP,                     // instruction pointer
+    SP,                     // stack pointer
+    REGISTER_SIZE
+} Registers;
+static int registers[REGISTER_SIZE];
 
-void eval(int instr) {
-    switch(instr) {
-        case HLT: {
-            running = false;
-            printf("done\n");
-            break;
+// instructions array
+int *instructions;
+
+// how many instructions were read from file
+int instructions_count = 0;
+
+// how much space is allocated for the instructions
+// 4 instructions by default
+int instructions_space = 4;
+
+static bool running = true;
+
+// if the IP is assigned by jmp instructions(such as IF, IFN), 
+// it should not increase 1 anymore
+bool is_jmp = false;
+
+// quick way to get SP and IP; usage: SP = ...
+#define SP (registers[SP])
+#define IP (registers[IP])
+
+// fetch current instruction set
+#define FETCH (instructions[IP])
+
+// prints the stack from A to B
+void print_stack() {
+    for (int i = 0; i < SP; i++) {
+        printf("0x%04d ", stack[i]);
+        if ((i + 1) % 4 == 0) {
+            printf("\n");
         }
-        case PSH: {
-            sp++;
-            stack[sp] = program[++ip];
-            break;
-        }
-        case POP: {
-            int val_popped = stack[sp--];
-            printf("popped: %d\n", val_popped);
-            break;
-        }
-        case ADD: {
-            int a = stack[sp--];
-            int b = stack[sp--];
-            int result = a + b;
-            sp++;
-            stack[sp] = result;
-            break;
-        }
+    }
+    if (SP != 0) {
+        printf("\n");
     }
 }
 
-int main() {
-    while(running) {
-        eval(fetch());
-        ip++;
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        printf("error: no input files\n");
+        return -1;
     }
+
+    char *filename = argv[1];
+
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        printf("error: could not read file `%s`\n", filename);
+        return -1;
+    }
+
+    // allocate space for instructions // TODO: remove (int*) or not
+    instructions = (int*)malloc(sizeof(*instructions) * instructions_space); // TODO: sizeof (int)
+
+    // read the binary file
+    int num;
+    int i = 0;
+    while(fscanf(file, "%d", &num) > 0) {
+        instructions[i] = num;
+        printf("%d\n", instructions[i]);
+        i++;
+        if (i >= instructions_count) {
+            instructions_space *= 2;
+            instructions = realloc(instructions, sizeof(*instructions) * instructions_space);
+        }
+    }
+    instructions_count = i;
+
+    fclose(file);
+   
+    // TODO: report a bug: where is IP initialization?
+    SP = -1;
+
+    while(running && IP < instructions_count) {
+        eval(FETCH);
+        if (!is_jmp) {
+            IP = IP+1;
+        }
+    }
+
+    free(instructions);
+
+    return 0;
 }
 
 
